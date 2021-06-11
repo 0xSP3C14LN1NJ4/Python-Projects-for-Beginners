51ae40f3f533442b69f5f266eb7cd9cb530be2fa
A4jni1n <0xSP3C14LN1NJ4@protonmail.com>
2021-06-10 20:14:54 -0400

    Update QRCodeGenerator.py

--- QRCodeGenerator.py
+++ QRCodeGenerator.py
@@ -1,7 +1,10 @@
 import qrcode
 
 # Data to put in the QR code
-img = qrcode.make("Helllo World!")
+data = input("Enter the data you want in the QR code: ")
+
+# Generate the QR code
+img = qrcode.make(data)
 
 # Save the QR code as an image
 img.save("image.png")
